a43c014aa9500cd00e823cbc475629433b6fa0b0
EC2 Default User <ec2-user@ip-172-31-40-34.ec2.internal>
2022-01-17 07:20:37 +0000

    kadai kansei

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,21 +5,28 @@ class BooksController < ApplicationController
   end
 
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 4. 詳細画面へリダイレクト
-    redirect_to book_path(book.id)
+   @book = Book.new(book_params)
+    if @book.save
+       redirect_to book_path(@book.id) , notice: "Book was successfully created."
+    else
+      @books = Book.all
+      flash.now[:alert]
+      render :index
+    end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+     if @book.update(book_params)
+        redirect_to book_path(@book.id) , notice: "Book was successfully created."
+     else
+      flash.now[:alert]
+      render :edit
+     end
   end
 
   def edit
